abde5841ca8f31e46a800cef655c6c36ff4f94d7
mohAttallah <mohmohmoud28@gmail.com>
2023-08-27 19:10:33 +0000

    finsh the lab still I have the test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -10,51 +10,83 @@ import Form from './Components/Form';
 import Results from './Components/Results';
 
 function App() {
-    const [respons, setRespons] = useState(null);
+    const [respons, setRespons] = useState({
+        headers: '',
+        data: "",
+    });;
     const [loading, setLoading] = useState(false);
     const [reqParams, setReqParams] = useState({
         method: '',
-        url: ""
+        url: "",
+        body: ""
     });
+
     function callApi(props) {
-        // mock output
         setReqParams(prevState => ({
             ...prevState,
             method: props.method,
             url: props.url,
+            body: props.body
         }))
-        setLoading(true);
-        axios({
-            method: props.method,
-            url: props.url,
-            
-        }).then(response => {
-            setRespons(response.data);
-            setLoading(false); // Stop loading
-        }).catch(err =>{
-            setRespons("Error");
-            setLoading(false); 
-        })
+
     }
+    useEffect(() => {
+        if (reqParams.method && reqParams.url) {
+            setLoading(true);
+            axios({
+                method: reqParams.method,
+                url: reqParams.url,
+                headers: {
+                    'Content-Type': 'application/json', // Set the content type to JSON
+                },
+                data: reqParams.body ? JSON.parse(reqParams.body) : null
+            }).then(response => {
+                //check if there is a headers on respons or not 
+                if (respons.headers && data.respons) {
+                    setRespons(prevState => ({
+                        ...prevState,
+                        headers: response.headers,
+                        data: response.data,
+                    }))
+                } else {
+                    setRespons(prevState => ({
+                        ...prevState,
+                        headers: "",
+                        data: response.data,
+                    }))
+                }
+                setLoading(false); // Stop loading
+            }).catch(err => {
+                if (err.response) {
+                    console.log('Response data:', err.response.data);
+                }
+                setRespons("Error");
+                setLoading(false);
+            })
+        }
+
+    }, [reqParams])
+
+
 
     return (
         <React.Fragment>
             <Header />
-            <Form handleApiCall={callApi} />
+            <Form className="form" handleApiCall={callApi} />
             <div className="grid-section">
                 <div className="left-section">
                     <div>Request Method: {reqParams.method}</div>
                     <div>URL: {reqParams.url}</div>
                 </div>
                 <div className="right-section">
-                    <Results data={respons} loading={loading} />
+                    <Results role="result" data={respons} loading={loading} />
                 </div>
             </div>
-
-
             <Footer />
         </React.Fragment>
     )
 }
 
 export default App;
+
+
--- src/_test_/app.test.jsx
+++ src/_test_/app.test.jsx
@@ -1,17 +1,60 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import Fetch from '../App'
+import '@testing-library/jest-dom'
+import { Form } from '../Components/Form';
+
 import App from '../App';
+const server = setupServer(
+    rest.get('/greeting', (req, res, ctx) => {
+        return res(ctx.json({
+            method: "get",
+            url: "sss",
+            body: "",
+        }))
+    }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+
+test('loads and displays greeting', async () => {
+    render(<Fetch url="/greeting" />)
+    const goButton = screen.getByText('GO!');
+
+    await waitFor(() => expect(goButton).toBeInTheDocument());
+    const resultsElement = screen.getByRole('result');
+    expect(resultsElement).toHaveTextContent('hello there');
+
+})
+
+// test('loads and displays greeting', async () => {
+//     render(<App />);
+//     fireEvent.click(screen.getByText('GO!'));
+//     expect(screen.getByText('Request Method:')).toBeTruthy();
+//     expect(screen.getByText('method')).toBeTruthy();
+//     expect(screen.getByText('hello there')).toBeInTheDocument();
+// }); 
+
+
 
 test('renders header', () => {
     render(<App />);
-    const headerElement = screen.getByText(/RESTy/i); // Update with actual header text
+    const headerElement = screen.getByText(/RESTy/i);
     expect(headerElement).toBeTruthy();
 });
 
 test('renders form', () => {
     render(<App />);
-    const formElement = screen.getByLabelText(/URL:/i); // Update with actual form label text
+    const formElement = screen.getByLabelText(/URL:/i);
     expect(formElement).toBeTruthy();
 });
+
+
+
 
 
