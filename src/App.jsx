2afb11f7e10229cdefe418cc52123c488564ff83
mohAttallah <mohmohmoud28@gmail.com>
2023-09-07 23:28:16 +0300

    add prev and next button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
 import './App.scss';
 import Header from './Components/Header';
@@ -13,10 +13,14 @@ import { reducer, initialState } from './reducer';
 function App() {
 
     const [state, dispatch] = useReducer(reducer, initialState);
+    const [previousLink, setPreviousLink] = useState(null);
+    const [nextLink, setNextLink] = useState(null);
+
 
     function callApi(props) {
         console.log(props)
         dispatch({ type: 'SET_REQUEST_PARAMS', payload: props });
+
     }
 
     useEffect(() => {
@@ -33,7 +37,17 @@ function App() {
             }).then(response => {
                 //check if there is a headers on respons or not 
                 if (response.headers && response.data) {
-                    
+                    const nextLinkHeader = response.headers['link'];
+                    if (nextLinkHeader) {
+                        const links = parseLinkHeader(nextLinkHeader);
+                        console.log(links)
+                        setNextLink(links.next);
+                        setPreviousLink(links.first);
+                    } else {
+                        setNextLink(null);
+                        setPreviousLink(null);
+                    }
+
                     dispatch({
                         type: 'API_REQUEST_SUCCESS',
                         payload: { headers: response.headers, data: response.data },
@@ -55,6 +69,17 @@ function App() {
 
     }, [state.reqParams])
 
+    function parseLinkHeader(linkHeader) {
+        const links = {};
+        linkHeader.split(',').forEach(part => {
+            const section = part.split(';');
+            if (section.length !== 2) return;
+            const url = section[0].replace(/<(.*)>/, '$1').trim();
+            const rel = section[1].replace(/rel="(.*)"/, '$1').trim();
+            links[rel] = url;
+        });
+        return links;
+    }
 
     return (
         <React.Fragment>
@@ -66,10 +91,11 @@ function App() {
                     <div>URL: {state.reqParams.url}</div>
                 </div>
                 <div className="right-section">
-                    <Results role="result" data={state.respons} loading={state.loading} />
+                    <Results role="result" data={state.respons} loading={state.loading} previousLink={previousLink} nextLink={nextLink} />
+
                 </div>
                 <div>
-                    <History handleApiCall={callApi}  data={{
+                    <History handleApiCall={callApi} data={{
                         url: state.reqParams.url,
                         methode: state.reqParams.method
                     }} />
